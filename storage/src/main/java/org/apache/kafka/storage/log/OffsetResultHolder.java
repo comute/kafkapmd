89e3fd7c0240dbada91620084c6abda79ade09cb
m1a2st <s7133700@gmail.com>
2024-12-09 01:06:13 +0800

    update the constructor check

--- storage/src/main/java/org/apache/kafka/storage/log/OffsetResultHolder.java
+++ storage/src/main/java/org/apache/kafka/storage/log/OffsetResultHolder.java
@@ -99,6 +99,10 @@ public class OffsetResultHolder {
                 Optional<Exception> exception,
                 Optional<TimestampAndOffset> timestampAndOffset
         ) {
+            if ((exception.isPresent() && timestampAndOffset.isPresent()) || 
+                    (exception.isEmpty() && timestampAndOffset.isEmpty())) {
+                throw new IllegalArgumentException("Either exception or timestampAndOffset should be present, but not both");
+            }
             this.exception = exception;
             this.timestampAndOffset = timestampAndOffset;
         }
